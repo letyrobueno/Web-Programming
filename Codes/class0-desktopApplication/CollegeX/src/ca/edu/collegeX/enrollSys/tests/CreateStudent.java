5df32224835bfebf30e60d4898b1081fe443f1f2
Letícia Rodrigues Bueno <leticia@madacenter.com>
2019-11-13 16:43:09 -0500

    Update code

--- Codes/class0-desktopApplication/CollegeX/src/ca/edu/collegeX/enrollSys/tests/CreateStudent.java
+++ Codes/class0-desktopApplication/CollegeX/src/ca/edu/collegeX/enrollSys/tests/CreateStudent.java
@@ -3,16 +3,16 @@ package ca.edu.collegeX.enrollSys.tests;
 import ca.edu.collegeX.enrollSys.model.Student;
 
 public class CreateStudent {
-	public static void main(String[] args) {
-		Student student = new Student();
-		student.setName("Jack");
-		student.setEmail("jack@gmail.com");
-		student.setAddress("Av. Sherbrooke, 5001");
-		student.setId((long) 3);
-		
-		System.out.println(student.getName());
-		System.out.println(student.getEmail());
-		System.out.println(student.getAddress());
-		System.out.println(student.getId());	
-	}
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Jack");
+        student.setEmail("jack@gmail.com");
+        student.setAddress("Av. Sherbrooke, 5001");
+        student.setId((long) 3);
+
+        System.out.println(student.getName());
+        System.out.println(student.getEmail());
+        System.out.println(student.getAddress());
+        System.out.println(student.getId());	
+    }
 }
